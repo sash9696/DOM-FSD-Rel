4cd268ea8063f6a38ef5158001bb87f3b5c95aba
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-07-06 23:02:48 +0530

    Add files via upload

---
+++ dom/selectors.js
@@ -0,0 +1,22 @@
+// //GET ELEMENT BY ID
+
+// // var headerTitle = document.getElementById("header-title");
+
+// // headerTitle.innerText = "hello";
+
+// //GETELEMENTSBYCLASSNAME
+
+// var items = document.getElementsByClassName("list-group-item");
+
+// console.log(items[1]);
+// items[1].textContent = "Hello World";
+// items[1].style.fontWeight = "bold";
+
+// // items.style.backgroundColor = "black";
+
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "black";
+// }
+// items[1].style.backgroundColor = "red";
+
+console.dir(document);
